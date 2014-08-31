5d084271493385c92b6c16dbb2b118f2b8995bb2
Adam <adam@ShopPC.(none)>
2014-08-31 15:17:13 -0500

    grow/shrink test

--- static/js/app/view/console-view.js
+++ static/js/app/view/console-view.js
@@ -3,9 +3,14 @@ var smApp = angular.module('smApp', []);
 
 smApp.controller('consoleView', function($scope) {
 
-	$scope.clickTest = function() {
+	$scope.clickGrow = function() {
 			$scope.ship.width = 60;
 			$scope.ship.height = 20;
-		}
+		};
+
+	$scope.clickShrink = function() {
+			$scope.ship.width = 30;
+			$scope.ship.height = 10;
+		};
 	
 });
